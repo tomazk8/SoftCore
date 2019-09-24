5519cf8856cbacffdd14a372712d09ffcc89b1c3
Tomaz <tomazk8@gmail.com>
2019-09-24 12:59:03 +0200

    Improved AOP Interception Added AOP Logging example as unit test Added access-control example as unit test

--- SoftCore.Interception/CompositeApplicationInterceptor.cs
+++ SoftCore.Interception/CompositeApplicationInterceptor.cs
@@ -20,7 +20,11 @@ namespace SoftCore.Interception
             interceptor = new Interceptor(this);
         }
 
-        internal void OnCallIntercepted(MemberInvocation memberInvocation)
+        internal void OnCallIntercepting(CallingMemberInvocation memberInvocation)
+        {
+            CallIntercepting?.Invoke(this, new CallInterceptingEventArgs(memberInvocation));
+        }
+        internal void OnCallIntercepted(CalledMemberInvocation memberInvocation)
         {
             CallIntercepted?.Invoke(this, new CallInterceptedEventArgs(memberInvocation));
         }
@@ -40,17 +44,27 @@ namespace SoftCore.Interception
             }
         }
 
+        public event EventHandler<CallInterceptingEventArgs> CallIntercepting;
         public event EventHandler<CallInterceptedEventArgs> CallIntercepted;
     }
 
-    public class CallInterceptedEventArgs : EventArgs
+    public class CallInterceptingEventArgs : EventArgs
     {
-        public CallInterceptedEventArgs(MemberInvocation memberInvocation)
+        public CallInterceptingEventArgs(CallingMemberInvocation memberInvocation)
         {
             this.MemberInvocation = memberInvocation;
         }
 
-        public MemberInvocation MemberInvocation { get; private set; }
+        public CallingMemberInvocation MemberInvocation { get; private set; }
+    }
+    public class CallInterceptedEventArgs : EventArgs
+    {
+        public CallInterceptedEventArgs(CalledMemberInvocation memberInvocation)
+        {
+            this.MemberInvocation = memberInvocation;
+        }
+
+        public CalledMemberInvocation MemberInvocation { get; private set; }
     }
 
     class Interceptor : IInterceptor
@@ -64,18 +78,51 @@ namespace SoftCore.Interception
 
         public void Intercept(IInvocation invocation)
         {
-            MemberInvocation memberInvocation = new MemberInvocation(invocation.Arguments, invocation.GenericArguments,
-                invocation.Method, invocation.ReturnValue, invocation.TargetType);
+            CallingMemberInvocation memberInvocation = new CallingMemberInvocation(invocation.Arguments, invocation.GenericArguments,
+                invocation.Method, invocation.TargetType);
 
-            compositeApplicationInterceptor.OnCallIntercepted(memberInvocation);
+            compositeApplicationInterceptor.OnCallIntercepting(memberInvocation);
 
             invocation.Proceed();
+
+            CalledMemberInvocation memberCalledInvocation = new CalledMemberInvocation(invocation.Arguments, invocation.GenericArguments,
+                invocation.Method, invocation.ReturnValue, invocation.TargetType);
+            compositeApplicationInterceptor.OnCallIntercepted(memberCalledInvocation);
         }
     }
 
-    public class MemberInvocation
+    public class CallingMemberInvocation
     {
-        public MemberInvocation(object[] arguments, Type[] genericArguments, MethodInfo method, 
+        public CallingMemberInvocation(object[] arguments, Type[] genericArguments, MethodInfo method, 
+            Type targetType)
+        {
+            this.Arguments = arguments;
+            this.GenericArguments = genericArguments;
+            this.Method = method;
+            this.TargetType = targetType;
+        }
+
+        /// <summary>
+        /// Gets the arguments that the method has been invoked with.
+        /// </summary>
+        public object[] Arguments { get; private set; }
+        /// <summary>
+        /// Gets the generic arguments of the method.
+        /// </summary>
+        public Type[] GenericArguments { get; private set; }
+        /// <summary>
+        /// Gets the System.Reflection.MethodInfo representing the method being invoked on the proxy.
+        /// </summary>
+        public MethodInfo Method { get; private set; }
+        /// <summary>
+        /// Gets the type of the target object for the intercepted method.
+        /// </summary>
+        public Type TargetType { get; private set; }
+    }
+
+    public class CalledMemberInvocation
+    {
+        public CalledMemberInvocation(object[] arguments, Type[] genericArguments, MethodInfo method,
             object returnValue, Type targetType)
         {
             this.Arguments = arguments;
---
+++ Testing/SoftCore.Interception.UnitTests/LoggingTests.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+using SoftCore.Composition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftCore.Interception.UnitTests
+{
+    public class LoggingTests
+    {
+        private List<LogEntry> log = new List<LogEntry>();
+
+        [Import]
+        private IClassA classA;
+
+        [Test]
+        public void TestLogging()
+        {
+            TypeCatalog catalog = new TypeCatalog(
+                typeof(ClassA),
+                typeof(ClassB));
+
+            CompositeApplication compositeApplication = new CompositeApplication(catalog);
+            CompositeApplicationInterceptor interceptor = new CompositeApplicationInterceptor(compositeApplication);
+            interceptor.CallIntercepted += Interceptor_CallIntercepted;
+
+            compositeApplication.SatisfyImportsOnInstance(this);
+            var result = classA.Convert(7);
+
+            Assert.AreEqual(log.Count, 2);
+
+            Assert.AreEqual(log[0].TargetType, typeof(ClassB));
+            Assert.AreEqual(log[0].TargetMethodName, nameof(ClassB.ConvertInternal));
+            Assert.IsTrue(log[0].ReturnValue is string);
+            Assert.AreEqual(log[0].ReturnValue, "8");
+            Assert.AreEqual(log[0].Parameters.Count(), 1);
+            Assert.IsTrue(log[0].Parameters.Single().Value is int);
+            Assert.AreEqual((int)log[0].Parameters.Single().Value, 7);
+
+            Assert.AreEqual(log[1].TargetType, typeof(ClassA));
+            Assert.AreEqual(log[1].TargetMethodName, nameof(ClassA.Convert));
+            Assert.IsTrue(log[1].ReturnValue is string);
+            Assert.AreEqual(log[1].ReturnValue, "8");
+            Assert.AreEqual(log[1].Parameters.Count(), 1);
+            Assert.IsTrue(log[1].Parameters.Single().Value is int);
+            Assert.AreEqual((int)log[1].Parameters.Single().Value, 7);
+        }
+
+        private void Interceptor_CallIntercepted(object sender, CallInterceptedEventArgs e)
+        {
+            List<MethodParameter> methodParameters = new List<MethodParameter>();
+
+            {
+                var tmpMethodParamsList = e.MemberInvocation.Method.GetParameters();
+
+                for (int i = 0; i < e.MemberInvocation.Arguments.Length; i++)
+                {
+                    methodParameters.Add(new MethodParameter
+                    {
+                        Name = tmpMethodParamsList[i].Name,
+                        Value = e.MemberInvocation.Arguments[i]
+                    });
+                }
+            }
+
+            LogEntry logEntry = new LogEntry
+            {
+                TargetMethodName = e.MemberInvocation.Method.Name,
+                TargetType = e.MemberInvocation.TargetType,
+                Parameters = methodParameters,
+                ReturnValue = e.MemberInvocation.ReturnValue
+            };
+
+            log.Add(logEntry);
+        }
+
+        #region Classes
+
+        public class LogEntry
+        {
+            public string TargetMethodName;
+            public Type TargetType;
+            public IEnumerable<MethodParameter> Parameters;
+            public object ReturnValue;
+        }
+        public class MethodParameter
+        {
+            public string Name;
+            public object Value;
+        }
+
+        public interface IClassA
+        {
+            string Convert(int value);
+        }
+
+        [Export(typeof(IClassA))]
+        public class ClassA : IClassA
+        {
+            [Import]
+            private IClassB classB;
+
+            private ClassA()
+            {
+            }
+
+            public string Convert(int value)
+            {
+                return classB.ConvertInternal(value);
+            }
+        }
+
+        public interface IClassB
+        {
+            string ConvertInternal(int value);
+        }
+
+        [Export(typeof(IClassB))]
+        public class ClassB : IClassB
+        {
+            private ClassB()
+            {
+            }
+
+            public string ConvertInternal(int value)
+            {
+                return (value + 1).ToString();
+            }
+        }
+        #endregion
+    }
+}
